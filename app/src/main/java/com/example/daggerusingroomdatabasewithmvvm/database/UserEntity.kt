e63297ab5ac390a96fedee395c0ef3d1a38239bc
ErroredPasta <ForeignServices3132@pm.me>
2022-08-09 00:08:06 +0900

    Room Database refactoring - Database instance를 Dagger로 관리하므로 getAppDatabaseInstance method 삭제 - UserEntity의 table name과 column name과 Database name을 const val로 선언

--- app/src/main/java/com/example/daggerusingroomdatabasewithmvvm/database/UserEntity.kt
+++ app/src/main/java/com/example/daggerusingroomdatabasewithmvvm/database/UserEntity.kt
@@ -3,10 +3,21 @@ package com.example.daggerusingroomdatabasewithmvvm.database
 import androidx.room.ColumnInfo
 import androidx.room.Entity
 import androidx.room.PrimaryKey
+import com.example.daggerusingroomdatabasewithmvvm.database.UserEntity.Companion.TABLE_NAME
 
-@Entity(tableName = "user_entity")
-class UserEntity(
-  @PrimaryKey(autoGenerate = true)@ColumnInfo(name = "id") val id : Int =0,
-  @ColumnInfo(name = "desc") val name:String
+@Entity(tableName = TABLE_NAME)
+data class UserEntity(
+    @PrimaryKey(autoGenerate = true)
+    @ColumnInfo(name = ID_COLUMN_NAME)
+    val id: Int = 0,
 
-)
+    @ColumnInfo(name = NAME_COLUMN_NAME)
+    val name: String
+) {
+    companion object {
+        const val TABLE_NAME = "user_entity"
+
+        const val ID_COLUMN_NAME = "id"
+        const val NAME_COLUMN_NAME = "desc"
+    }
+}
